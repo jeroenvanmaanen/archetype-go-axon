3640c18310e6862dcdfc2cfc13a4a54981933290
Jeroen van Maanen <jeroen@leialearns.org>
2020-04-03 08:38:07 +0200

    Added ROOT_PRIVATE_KEY to "settings-sample.sh".

--- src/etc/settings-sample+jeroen.sh
+++ src/etc/settings-sample+jeroen.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/false
 
 DOCKER_REPOSITORY='jeroenvm'
+EXAMPLE_IMAGE_VERSION='0.0.1-SNAPSHOT'
 UI_SERVER_PORT='3000'
-API_SERVER_PORT='8080'
+API_SERVER_PORT='8181'
 AXON_SERVER_PORT='8024'
 AXON_VERSION='4.3.1'
 ELASTIC_SEARCH_VERSION='7.6.1'
+ROOT_PRIVATE_KEY='/Users/jeroen/.ssh/id_rsa'
 
-#EXTRA_VOLUMES='
-#      -
-#        type: bind
-#        source: /Users/jeroen/src/ledger-axon
-#        target: /Users/jeroen/src/ledger-axon'
+EXTRA_VOLUMES="
+      -
+        type: bind
+        source: ${PROJECT}
+        target: ${PROJECT}"
