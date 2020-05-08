7d4f1026e68bd11ce627c60d1172743737ca4ee9
Jeroen van Maanen <jeroen@leialearns.org>
2020-05-08 08:20:04 +0200

    Added comments to "authentication/projection.go".

--- src/pkg/authentication/projection.go
+++ src/pkg/authentication/projection.go
@@ -7,12 +7,15 @@ import (
     grpc_example "github.com/jeroenvm/archetype-go-axon/src/pkg/grpc/example"
 )
 
+// Redeclare event types, so that they can be extended with event handler methods.
+type CredentialsAddedSourceEvent   struct { grpc_example.CredentialsAddedEvent }
+type CredentialsRemovedSourceEvent struct { grpc_example.CredentialsRemovedEvent }
+
+// Projection
+
 type Projection struct {
     Credentials map[string]string
 }
-
-type CredentialsAddedSourceEvent   struct { grpc_example.CredentialsAddedEvent }
-type CredentialsRemovedSourceEvent struct { grpc_example.CredentialsRemovedEvent }
 
 func RestoreProjection(aggregateIdentifier string, clientConnection *axon_utils.ClientConnection) *Projection {
     projection := Projection{
@@ -26,6 +29,9 @@ func (projection *Projection) Apply(event axon_utils.SourceEvent) {
     event.ApplyTo(projection)
 }
 
+// Map an event name as stored in AxonServer to an object that has two aspects:
+// 1. It is a proto.Message, so it can be unmarshalled from the Axon event
+// 2. It is an axon_utils.SourceEvent, so it can be applied to a projection
 func prepareUnmarshal(payloadType string) (sourceEvent axon_utils.SourceEvent) {
     log.Printf("Credentials Projection: Payload type: %v", payloadType)
     switch payloadType {
@@ -35,6 +41,8 @@ func prepareUnmarshal(payloadType string) (sourceEvent axon_utils.SourceEvent) {
     }
     return sourceEvent
 }
+
+// Event Handlers
 
 func (sourceEvent *CredentialsAddedSourceEvent) ApplyTo(projectionWrapper interface{}) {
     projection := projectionWrapper.(Projection)
